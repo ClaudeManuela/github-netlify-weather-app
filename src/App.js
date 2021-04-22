30835c9100fa91ca42646a84368d41ec46e654e4
ClaudeManuela <Manuelaekotto@gmail.com>
2021-04-21 21:02:49 -0600

    Added attribution at the end

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Search from "./Search";
+import Attribution from "./Attribution"
 
 export default function App() {
   return (
+    <div>
     <div className="App">
       <h1> Weather app</h1>
       <Search />
+    </div>
+    <Attribution />
     </div>
   );
 }
---
+++ src/Attribution.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Attribution = () => {
+    return (
+        <div className="Attribution">
+       <a href ="https://github.com/ClaudeManuela/github-netlify-weather-app"> Open source code</a> by <a href="https://github.com/ClaudeManuela">Claude Manuela Ekotto Eboa</a>
+            
+        </div>
+    )
+}
+
+export default Attribution
